f040882a6e352418a8d250c1c1a1c45b2abfa446
Antonio-naddeo <antonio.naddeomf@gmail.com>
2021-11-16 15:31:56 -0300

    setup grappelli

--- adminboost/settings.py
+++ adminboost/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli', #this line has to be before 'django.contrib.admin'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
